c9abd9130126817033bbb59b64e48d6372c93d49
BUI DINH NGUYEN <buinguyen.2521996@gmail.com>
2022-09-21 10:26:40 +0700

    BT-REACT

---
+++ btreact/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from './Components/BaiTapThucHanhLayout/Banner'
+import Footer from './Components/BaiTapThucHanhLayout/Footer'
+import Header from './Components/BaiTapThucHanhLayout/Header'
+import Item from './Components/BaiTapThucHanhLayout/Item'
+
+export default function App() {
+  return (
+    <div>
+        <Header />
+        <Banner />
+        <Item />
+        <Footer />
+    </div>
+  )
+}
